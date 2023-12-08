2006f5f262f887b0df0ed9fe2f9f54c684e5e09b
Rayhan Yovi <yovihan@gmail.com>
2023-12-08 15:00:06 +0700

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between bg-slate-100 p-24">
+      
+      <div className='flex flex-row'>
+      <div className='flex flex-col text-black'>
+        <p>Hello there! 👋 My name is</p>
+        <h1 className='text-4xl'>Rayhan Yovi,</h1>
+        <h1>Fullstack Engineer</h1>
+        <p>Crafting unforgettable digital experiences and building innovative web solutions is my forte. Welcome to my creative world, where every line of code tells a story, and every interface is a work of art</p>
+        <div>
+          <ul>
+            <ii>
+
+            </ii>
+          </ul>
+        </div>
+      </div>
+
+      <img src=''/>
+      </div>
+      
+    </main>
+  )
+}
